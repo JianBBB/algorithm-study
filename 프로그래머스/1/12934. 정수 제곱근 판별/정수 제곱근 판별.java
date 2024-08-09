74c4abc95132465f87bfb1e04939f70bbdf1633b
JianBBB <76256461+JianBBB@users.noreply.github.com>
2024-08-09 10:45:11 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 98.25 ms, Memory: 70.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long solution(long n) {
+        
+        for(long i = 1; i <= n ; i++) {
+            if((n / i == i) && (n%i==0)){
+                return (i+1) * (i+1);
+            }
+        }
+        return -1;
+    }
+}
